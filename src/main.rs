a81802a56d631e4b06dbe54fbc30e33de91bb119
Luc Bergeron <psyluche@gmail.com>
2019-12-26 14:29:30 -0500

    First two parts done.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate sdl2;
+extern crate gl;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+    //OpenGL attributes...
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
+    let window = video_subsystem.window("Rust OpenGL tutorial.", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.0, 0.3, 0.5, 1.0);
+    }
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            //Handle user input...
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        //Handle rendering here.
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
 }
